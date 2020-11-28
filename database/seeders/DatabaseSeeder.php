9c1deb9143197033e094e1aa0594918ca8c3138a
mdutt247 <59934065+mdutt247@users.noreply.github.com>
2020-11-28 14:51:41 +0530

    Addded missing seed for 'post_tag' table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,15 @@ class DatabaseSeeder extends Seeder
         \App\Models\Video::factory(500)->create();
         \App\Models\Tag::factory(50)->create();
         \App\Models\Post::factory(1500)->create();
+
+        // post_tag table has no Model so to seed it we need our own logic
+        for ($i = 0; $i < 1500; $i++) {
+            DB::table('post_tag')->insertOrIgnore([
+                'post_id' => mt_rand(1, 1500),
+                'tag_id' => mt_rand(1, 50),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 }
