71c6309bfc428b56612e704b217ea9a858913f39
mdutt247 <59934065+mdutt247@users.noreply.github.com>
2020-12-01 00:38:02 +0530

    Fixed Tag->Posts api route

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\Api\CategoryApiController;
 use App\Http\Controllers\Api\CommentApiController;
 use App\Http\Controllers\Api\PostApiController;
+use App\Http\Controllers\Api\TagApiController;
 use App\Http\Controllers\Api\UserApiController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +30,8 @@ Route::get('categories/{id}/posts', [CategoryApiController::class, 'posts']);
 Route::get('posts', [PostApiController::class, 'index']);
 Route::get('posts/{id}', [PostApiController::class, 'show']);
 Route::get('posts/{id}/comments', [PostApiController::class, 'comments']);
+
+Route::get('tags/{id}/posts', [TagApiController::class, 'posts']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('comments/posts', [CommentApiController::class, 'store']);
