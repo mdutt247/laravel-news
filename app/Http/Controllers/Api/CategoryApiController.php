60fb7ecc73ab3242b3386fdf6e9a7a967c4dfcdb
mdutt247 <59934065+mdutt247@users.noreply.github.com>
2021-02-22 11:20:41 +0530

    added in comments how to use Sanctum's tokenCan in a controller

--- app/Http/Controllers/Api/CategoryApiController.php
+++ app/Http/Controllers/Api/CategoryApiController.php
@@ -13,11 +13,20 @@ class CategoryApiController extends Controller
 {
     public function index()
     {
+        //See if user has certain abilities
+        // if (!auth()->user()->tokenCan('categories-list')) {
+        //     abort(403, 'Unauthorized');
+        // }
+
         $categories = Category::all();
+        // if ($categories == null) {
+        //     return response()->json(['message' => 'Not Found!'], 404);
+        // }
         return CategoryResource::collection($categories);
     }
 
-    public function posts($id){
+    public function posts($id)
+    {
         $posts = Post::where('category_id', $id)->orderBy('id', 'desc')->paginate();
         return PostResource::collection($posts);
     }
